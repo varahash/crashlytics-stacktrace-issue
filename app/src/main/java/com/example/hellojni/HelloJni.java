13ae24297979fd4429b84229f3d124b0f2aed651
Mykola Varahash <mvarahash@conceptsoft.io>
2020-04-18 13:03:11 +0300

    Add new 'crashMe' method

--- app/src/main/java/com/example/hellojni/HelloJni.java
+++ app/src/main/java/com/example/hellojni/HelloJni.java
@@ -17,7 +17,6 @@ package com.example.hellojni;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,38 +30,17 @@ public class HelloJni extends AppCompatActivity {
          * function.
          */
         setContentView(R.layout.activity_hello_jni);
-        final TextView tv = findViewById(R.id.hello_textview);
         
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText( stringFromJNI() );
+                crashMe();
             }
         });
     }
-    /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
-     * with this application.
-     */
-    public native String  stringFromJNI();
+    
+    public native void crashMe();
 
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-    public native String  unimplementedStringFromJNI();
-
-    /* this is used to load the 'hello-jni' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.example.hellojni/lib/libhello-jni.so at
-     * installation time by the package manager.
-     */
     static {
         System.loadLibrary("hello-jni");
     }
