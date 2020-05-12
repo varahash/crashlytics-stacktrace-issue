cde8d5583a4e707156fdc3a5a265d30bca93d8cb
Mykola Varahash <mvarahash@conceptsoft.io>
2020-05-12 09:54:27 +0300

    Update Crashlytics plugin and enable debug symbols

--- app/Android.mk
+++ app/Android.mk
@@ -6,8 +6,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := hello-jni
 LOCAL_SRC_FILES := $(JNI_SRC_PATH)/hello-jni.c
 
-# workaround for https://github.com/firebase/firebase-android-sdk/issues/1396
-# so that *.cSYM will be generated based on 'elf' instead of 'dwarf'
-LOCAL_CFLAGS += -g0
-
 include $(BUILD_SHARED_LIBRARY)
